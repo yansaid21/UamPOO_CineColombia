2a9a8ceab80cd165fc6d8e8ae4517c06a3c60f37
yansaid21 <yansaid21@gmail.com>
2022-05-14 13:59:14 -0500

    first commit

---
+++ Servidor/CineColombia/src/main/java/CineColombia/CineColombia/CineColombiaApplication.java
@@ -0,0 +1,13 @@
+package CineColombia.CineColombia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineColombiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineColombiaApplication.class, args);
+	}
+
+}
---
+++ Servidor/CineColombia/src/test/java/CineColombia/CineColombia/CineColombiaApplicationTests.java
@@ -0,0 +1,13 @@
+package CineColombia.CineColombia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineColombiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
